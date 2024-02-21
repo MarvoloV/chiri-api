4f7f33a77f012d5e222f85d5396ca122a6f46ee2
Jorge Luis Adco Ccahuay <jorgead0812@gmail.com>
2024-02-20 20:57:01 -0500

    feature:create services registerUser and activateUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
+import { randomInt, createHash } from 'crypto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const user = this.userRepository.create(createUserDto);
+      user.password = bcrypt.hashSync(this.generateRandomPassword(), 10);
 
-  findAll() {
-    return `This action returns all auth`;
+      user.token_verify = this.generateHash(createUserDto.email);
+      await this.userRepository.save(user);
+      delete user.password;
+      delete user.token_verify;
+      return user;
+    } catch (error) {
+      this.handleDBerrors(error);
+    }
   }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async activateUser(id: string) {
+    const user = await this.userRepository.findOneBy({ token_verify: id });
+    if (!user) {
+      throw new BadRequestException('token invalido');
+    }
+    user.isActive = true;
+    await this.userRepository.save(user);
   }
+  private generateRandomPassword() {
+    const charset =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let password = '';
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+    for (let i = 0; i < 12; i++) {
+      const randomIndex = randomInt(0, charset.length);
+      password += charset[randomIndex];
+    }
+
+    return password;
   }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  private generateHash(email: string): string {
+    const hash = createHash('sha256').update(email).digest('hex');
+    return hash;
+  }
+  private handleDBerrors(error: any) {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail);
+    }
+    console.log(error);
+    throw new InternalServerErrorException('Revisar los logs del servidor');
   }
 }
--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,1 +1,24 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column('text', { unique: true })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  full_name: string;
+
+  @Column('boolean', { default: false })
+  isActive: boolean;
+
+  @Column('text')
+  phone: string;
+
+  @Column('text')
+  token_verify: string;
+}
